b6d02585b6cd81707c356d3f9906275fde64da56
Philip Clarke <clarkep19@gmail.com>
2023-10-31 20:29:47 -0400

    trying to add character and movement

---
+++ FinalProject/Assets/scripts/fighterMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fighterMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float displacement;
+    public Rigidbody2D fighter;
+    Vector2 initial;
+    public Animator animator;
+    public float jump; // for jump
+
+
+    void Start()
+    {
+        fighter = GetComponent<Rigidbody2D>();
+        initial= fighter.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if ((Input.GetKey(KeyCode.RightArrow))){
+                animator.SetFloat("speed",1.0f);
+                initial.x=initial.x+displacement;
+                
+        }
+        
+        if (Input.GetKey(KeyCode.Space)){
+            //for jump
+            fighter.AddForce(new Vector2(fighter.velocity.x,jump));
+            
+        }
+       
+        if (!Input.anyKey)
+            animator.SetFloat("speed",0);
+        
+
+            
+
+        fighter.MovePosition(initial);
+    }
+}
