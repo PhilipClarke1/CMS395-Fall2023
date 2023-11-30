96a1ad150f8587db4c5a8361992d813d1efde9e7
Philip Clarke <clarkep19@gmail.com>
2023-11-30 00:34:06 -0500

    added damage taken for the enemy

--- FinalProject/Assets/scripts/enemyHealth.cs
+++ FinalProject/Assets/scripts/enemyHealth.cs
@@ -1,25 +1,54 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class enemyHealth : MonoBehaviour
 {
-
-    public int maxHealth;
+    public int maxHealth = 100;
     public int currentHealth;
-    // Start is called before the first frame update
+    public Animator animator;
+    public SpriteRenderer spriteRenderer; // Reference to the SpriteRenderer
+
     void Start()
     {
         currentHealth = maxHealth;
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(int damage)
     {
+        currentHealth -= damage;
 
-            
+        // Play damage animation  
+        animator.SetTrigger("Hurt");
 
-        
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("The enemy is dead");
+
+        // Show death animation 
+        animator.SetBool("IsDead", true);
+
+        // Start the fade-out coroutine
+        StartCoroutine(FadeOut(3f)); // 1 second fade-out time
+    }
+
+    IEnumerator FadeOut(float fadeTime)
+    {
+        float startAlpha = spriteRenderer.color.a;
+
+        for (float t = 0; t < 1; t += Time.deltaTime / fadeTime)
+        {
+            Color newColor = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, Mathf.Lerp(startAlpha, 0, t));
+            spriteRenderer.color = newColor;
+            yield return null;
+        }
+
+        // Disable or destroy the enemy after fade-out
+        gameObject.SetActive(false); // or Destroy(gameObject);
     }
 }
--- FinalProject/Assets/scripts/fighterMovement.cs
+++ FinalProject/Assets/scripts/fighterMovement.cs
@@ -7,9 +7,20 @@ public class FighterMovement : MonoBehaviour
     public float moveSpeed = 5.0f;    // speed of fighter
     public Rigidbody2D fighter;
     public Animator animator;
-    public float jumpForce = 1f;    
+    public float jumpForce = 1f;   
+
+    public Transform attackpoint; 
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+
+    public int attackOneDamage = 25;
+    public int attackTwoDamage = 30;
+    public int attackThreeDamage = 40;
 
     private bool isGrounded;          // To know if the fighter is on the ground
+
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
 
     void Start()
     {
@@ -18,6 +29,15 @@ public class FighterMovement : MonoBehaviour
 
     void Update()
     {   
+
+        if(Time.time >= nextAttackTime){
+
+            if(Input.GetKeyDown(KeyCode.K)){
+                HandleAttack();
+                nextAttackTime = Time.time + 1f /attackRate;
+            }
+
+        }
         AnimateMovement();  
         HandleMovement();
         HandleJump();
@@ -27,25 +47,65 @@ public class FighterMovement : MonoBehaviour
 
     }
 
+//     void OnDrawGizmos() {
+//     Gizmos.color = Color.red;
+//     Gizmos.DrawWireSphere(transform.position, 0.5f); // Draws a red sphere with a radius of 0.5 units
+// }
+
     void HandleAttack(){
 
         if(Input.GetKeyDown(KeyCode.K)){
             PerformAttack();
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackpoint.position, attackRange, enemyLayers);
+
+            // apply damage
+            foreach(Collider2D enemy in hitEnemies) {
+
+
+            enemy.GetComponent<enemyHealth>().TakeDamage(attackOneDamage);
+
         }
+        }
+        
+
+        
+    }
+
+    void OnDrawGizmosSelected(){
+        if(attackpoint == null){
+            return;
+        }   
+        Gizmos.DrawWireSphere(attackpoint.position, attackRange);
     }
 
     void HandleAttack2(){
 
         if(Input.GetKeyDown(KeyCode.L)){
             PerformAttack2();
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackpoint.position, attackRange, enemyLayers);
+
+            // apply damage
+            foreach(Collider2D enemy in hitEnemies) {
+
+
+            enemy.GetComponent<enemyHealth>().TakeDamage(attackTwoDamage);
         }
     }
 
+    }
     void HandleAttack3(){
 
         if(Input.GetKeyDown(KeyCode.L) && Input.GetKeyDown(KeyCode.K)){
             PerformAttack3();
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackpoint.position, attackRange, enemyLayers);
+
+            // apply damage
+            foreach(Collider2D enemy in hitEnemies) {
+
+
+            enemy.GetComponent<enemyHealth>().TakeDamage(attackThreeDamage);
         }
+    }
     }
 
 
